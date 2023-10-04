0d23ac6d04a0bd3ae3dd22964165f4c70840a983
Bhavesh Zalke <zalkebhavesh@gmail.com>
2023-10-04 07:37:45 +0530

    1.1

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ function gsapForHeader() {
     })
 }
 gsapForHeader()
+
+
+gsap.from("##page3", {
+    y: 10,
+    x:-10,
+    scale: 0,
+    opacity:0,
+    ease: "back.in",
+    opacity: 0,
+    duration: 2,
+    stagger: 0.1
+})
